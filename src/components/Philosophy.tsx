364c147eb6aee67ad75d1c671308c74ce345dd9c
ArkoRoyBadhon <arkoroyb@gmail.com>
2025-02-01 23:03:20 +0600

    fix a issue

--- src/components/Philosophy.tsx
+++ src/components/Philosophy.tsx
@@ -1,8 +1,36 @@
-import React from "react";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+'use client'
+import React, { useEffect, useRef } from "react";
 import SectionTitle from "./SectionTitle";
 import Image from "next/image";
+import gsap from "gsap";
 
 const Philosophy = () => {
+  const cardRefs = useRef<HTMLElement[]>([]);
+
+  useEffect(() => {
+    cardRefs.current.forEach((card, index) => {
+      const delay = index * 2;
+      gsap.fromTo(
+        card,
+        {
+          x: -100,
+          y: 100,
+          opacity: 0,
+        },
+        {
+          x: 0,
+          y: 50,
+          opacity: 1,
+          duration: 1.5, 
+          ease: "power2.out", 
+          delay: delay, 
+        }
+      );
+    });
+  }, []);
+
+
   return (
     <section className="screen_size">
       <SectionTitle
@@ -26,7 +54,8 @@ const Philosophy = () => {
         />
       </figure>
       <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[30px]">
-        <li className="translate-y-[20%] z-20 h-full md:space-y-sm space-y-[8px] bg-[#F8FCFF] rounded-[20px] md:p-md p-[32px] ">
+      {/* ref={(el: any) => el && iconRefs.current.push(el)} */}
+        <li ref={(el:any) => el && cardRefs.current.push(el)} className="translate-y-[20%] z-20 h-full md:space-y-sm space-y-[8px] bg-[#F8FCFF] rounded-[20px] md:p-md p-[32px] ">
           <figure className="w-fit rounded-full">
             <Image
               src="https://cdn.sanity.io/images/6jywt20u/production/28029da89383a59e47420ee46b7e4c364051b45f-50x50.svg?auto=format"
@@ -46,7 +75,7 @@ const Philosophy = () => {
             payment functions with our comprehensive suite of solutions.
           </p>
         </li>
-        <li className="translate-y-[20%] z-20 h-full md:space-y-sm space-y-[8px] bg-[#F8FCFF] rounded-[20px] md:p-md p-[32px] ">
+        <li ref={(el:any) => el && cardRefs.current.push(el)} className="translate-y-[20%] z-20 h-full md:space-y-sm space-y-[8px] bg-[#F8FCFF] rounded-[20px] md:p-md p-[32px] ">
           <figure className="w-fit rounded-full">
             <Image
               src="https://cdn.sanity.io/images/6jywt20u/production/36c4da4283252fda5dce13c46ea3e06a5312218c-50x51.png?w=50&auto=format"
@@ -67,7 +96,7 @@ const Philosophy = () => {
             Data.
           </p>
         </li>
-        <li className="translate-y-[20%] z-20 h-full md:space-y-sm space-y-[8px] bg-[#F8FCFF] rounded-[20px] md:p-md p-[32px] ">
+        <li ref={(el:any) => el && cardRefs.current.push(el)} className="translate-y-[20%] z-20 h-full md:space-y-sm space-y-[8px] bg-[#F8FCFF] rounded-[20px] md:p-md p-[32px] ">
           <figure className="w-fit rounded-full">
             <Image
               src="https://cdn.sanity.io/images/6jywt20u/production/1966d94a29ffe1673fd510327ba0eb409f82b680-50x50.svg?w=50&auto=format"
